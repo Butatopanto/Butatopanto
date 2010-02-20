d3e4f51881fbf9c93e0bf386d753d7ee8279c7f4
Sandra Sieroux <s.sieroux@gmx.net>
2010-02-20 20:38:10 +0100

    Unnötige Reload-Solve Funktionen weggeschmissen

--- grails-app/controllers/net/butatopanto/vocable/QuestionController.groovy
+++ grails-app/controllers/net/butatopanto/vocable/QuestionController.groovy
@@ -12,15 +12,7 @@ class QuestionController {
       def vocableCount = Vocable.count();
       def randomVocableIndex = new Random().nextInt(vocableCount)
       def vocable = Vocable.list().getAt(randomVocableIndex)
-      params.vocable = vocable;
       def question= new Question(vocable:vocable)
       [questionInstance: question]
-    }
-
-    def solve = {
-      def vocableId = params.vocableId
-      def vocable = Vocable.get(vocableId)
-      def questionInstance = new Question(vocable: vocable)
-      render(view: "solution", model: [questionInstance: questionInstance])
     }
 }
