3488bb04478e3c01aa8a9ca4cf46ec249cd3cc85
Sandra Sieroux <s.sieroux@gmx.net>
2010-02-28 12:45:00 +0100

    VocableService refactored

--- grails-app/services/net/butatopanto/vocable/VocableService.groovy
+++ grails-app/services/net/butatopanto/vocable/VocableService.groovy
@@ -3,10 +3,16 @@ package net.butatopanto.vocable
 class VocableService {
 
   boolean transactional = false
+  def random = new Random();
 
   def getRandomVocable() {
-    def vocableCount = Vocable.count();
-    def randomVocableIndex = new Random().nextInt(vocableCount)
-    Vocable.list().getAt(randomVocableIndex)
+    def allVocables = getAllVocables()
+    def vocableCount = allVocables.size()
+    def randomVocableIndex = random.nextInt(vocableCount)
+    allVocables[randomVocableIndex]
+  }
+
+  def getAllVocables() {
+    Vocable.list()
   }
 }
--- test/unit/net/butatopanto/vocable/VocableServiceTests.groovy
+++ test/unit/net/butatopanto/vocable/VocableServiceTests.groovy
@@ -3,15 +3,15 @@ package net.butatopanto.vocable
 import grails.test.*
 
 class VocableServiceTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
+  protected void setUp() {
+    super.setUp()
+  }
+
+  void testSomething() {
+     def service = new VocableService();
+  }
 
     protected void tearDown() {
-        super.tearDown()
-    }
-
-    void testSomething() {
-
+      super.tearDown()
     }
 }
