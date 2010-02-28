bd7ec164551214cfccdb01f657f3f98a206485c7
Sandra Sieroux <s.sieroux@gmx.net>
2010-02-28 11:27:31 +0100

    UnitTests für Vocable mit mockForConstraintsTests

--- test/unit/net/butatopanto/vocable/VocableTests.groovy
+++ test/unit/net/butatopanto/vocable/VocableTests.groovy
@@ -5,13 +5,26 @@ import grails.test.*
 class VocableTests extends GrailsUnitTestCase {
   protected void setUp() {
     super.setUp()
+    mockForConstraintsTests(Vocable, [new Vocable()])
+  }
+
+  void testNewVocableIsNotValid() {
+    assertFalse new Vocable().validate();
+  }
+
+  void testVocableWithMeaningAndKanaIsValid() {
+    assertTrue new Vocable(meaning: "sehen", kana: "??").validate();
+  }
+
+  void testVocableWithMeaningOnlyIsNotValid() {
+    assertFalse new Vocable(meaning: "sehen").validate();
+  }
+
+  void testVocableWithKanaOnlyIsNotValid() {
+    assertFalse new Vocable(kana: "??").validate();
   }
 
   protected void tearDown() {
     super.tearDown()
-  }
-
-  void testSomething() {
-    assertEquals Vocable.constraints(0), "Meaning"
   }
 }
