1aa0ce19902b3f7045a81ebab9d698ee932b3e95
Urs@Urs-PC-Neu <Urs@Urs-PC-Neu>
2010-06-05 21:28:10 +0200

    Sauberer Code im Skript

--- scripts/MakeDistribution.groovy
+++ scripts/MakeDistribution.groovy
@@ -2,17 +2,21 @@ includeTargets << grailsScript("Init")
 includeTargets << grailsScript("_GrailsWar")
 includeTargets << grailsScript("_GrailsBootstrap")
 
-target(main: "Builds a war file and uploads it to Google code") {
+target(main: "Builds a war file and publishes it") {
   war()
+  uploadToGoogleCode()
+}
+
+target(uploadToGoogleCode:"Takes the war file and publishes it on the Google page") {
   def console = System.console();
   def username = console.readLine("Username? ")
   def password = console.readLine("Password? ")
   def summary = console.readLine("Content Summary? ")
   def filename = createFilename()
-  def sourcefile = "target\\"+filename
+  def sourcefile = "target\\" + filename
   def targetfile = filename
   taskdef(classname: "net.bluecow.googlecode.ant.GoogleCodeUploadTask", classpath: "path/to/ant-googlecode.jar", name: "upload")
-  upload(username: username, password: password, projectname: "butatopanto", summary: summary, filename: sourcefile, targetfilename: targetfile, labels:"OpSys-All")
+  upload(username: username, password: password, projectname: "butatopanto", summary: summary, filename: sourcefile, targetfilename: targetfile, labels: "OpSys-All")
 }
 
 private def createFilename() {
