227e50c80f2361fceef4235173e7684d17507e2a
Urs@Urs-PC-Neu <Urs@Urs-PC-Neu>
2010-06-05 15:43:50 +0200

    Upload-Script und Bibliotheken dafür

---
+++ scripts/MakeDistribution.groovy
@@ -0,0 +1,24 @@
+includeTargets << grailsScript("Init")
+includeTargets << grailsScript("_GrailsWar")
+includeTargets << grailsScript("_GrailsBootstrap")
+
+target(main: "Builds a war file and upload it to Google code") {
+  war()
+  def console = System.console();
+  def username = console.readLine("Username? ")
+  def password = console.readLine("Password? ")
+  def summary = console.readLine("Content Summary? ")
+  def filename = createFilename()
+  def sourcefile = "target\\"+filename
+  def targetfile = filename
+  ant.taskdef(classname: "net.bluecow.googlecode.ant.GoogleCodeUploadTask", classpath: "path/to/ant-googlecode.jar", name: "upload")
+  upload(username: username, password: password, projectname: "butatopanto", summary: summary, filename: sourcefile, targetfilename: targetfile, labels:"OpSys-All")
+}
+
+private def createFilename() {
+  loadApp()
+  def version = grailsApp.metadata.'app.version'
+  "butatopanto-" + version + ".war"
+}
+
+setDefaultTarget(main)
