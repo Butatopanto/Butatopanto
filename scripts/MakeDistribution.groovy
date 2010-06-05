34c5d3a2b8744803c8d44bd4479153f97c45187c
Urs@Urs-PC-Neu <Urs@Urs-PC-Neu>
2010-06-05 21:31:22 +0200

    Klassenpfad unabhängig vom Projekt direkt im Skript

--- scripts/MakeDistribution.groovy
+++ scripts/MakeDistribution.groovy
@@ -15,7 +15,7 @@ target(uploadToGoogleCode: "Takes the war file and publishes it on the Google pa
   def filename = createFilename()
   def sourcefile = "target\\" + filename
   def targetfile = filename
-  taskdef(classname: "net.bluecow.googlecode.ant.GoogleCodeUploadTask", classpath: "path/to/ant-googlecode.jar", name: "upload")
+  taskdef(name: "upload", classname: "net.bluecow.googlecode.ant.GoogleCodeUploadTask", classpath: "lib/ant-googlecode-0.0.2.jar")
   upload(username: username, password: password, projectname: "butatopanto", summary: summary, filename: sourcefile, targetfilename: targetfile, labels: "OpSys-All")
 }
 
