81038a12c003cf59f3f995f6be651df26315817a
Urs@Urs-PC-Neu <Urs@Urs-PC-Neu>
2010-06-05 21:20:23 +0200

    Cleanup im Script

--- scripts/MakeDistribution.groovy
+++ scripts/MakeDistribution.groovy
@@ -2,7 +2,7 @@ includeTargets << grailsScript("Init")
 includeTargets << grailsScript("_GrailsWar")
 includeTargets << grailsScript("_GrailsBootstrap")
 
-target(main: "Builds a war file and upload it to Google code") {
+target(main: "Builds a war file and uploads it to Google code") {
   war()
   def console = System.console();
   def username = console.readLine("Username? ")
@@ -11,7 +11,7 @@ target(main: "Builds a war file and upload it to Google code") {
   def filename = createFilename()
   def sourcefile = "target\\"+filename
   def targetfile = filename
-  ant.taskdef(classname: "net.bluecow.googlecode.ant.GoogleCodeUploadTask", classpath: "path/to/ant-googlecode.jar", name: "upload")
+  taskdef(classname: "net.bluecow.googlecode.ant.GoogleCodeUploadTask", classpath: "path/to/ant-googlecode.jar", name: "upload")
   upload(username: username, password: password, projectname: "butatopanto", summary: summary, filename: sourcefile, targetfilename: targetfile, labels:"OpSys-All")
 }
 
