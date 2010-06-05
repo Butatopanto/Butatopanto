f28fd0ae0a7780624be48127a2d9b8b5bf62939c
Urs@Urs-PC-Neu <Urs@Urs-PC-Neu>
2010-06-05 21:33:05 +0200

    Grails übernimmt den Klassenpfad für uns

--- scripts/MakeDistribution.groovy
+++ scripts/MakeDistribution.groovy
@@ -15,7 +15,7 @@ target(uploadToGoogleCode: "Takes the war file and publishes it on the Google pa
   def filename = createFilename()
   def sourcefile = "target\\" + filename
   def targetfile = filename
-  taskdef(name: "upload", classname: "net.bluecow.googlecode.ant.GoogleCodeUploadTask", classpath: "lib/ant-googlecode-0.0.2.jar")
+  taskdef(name: "upload", classname: "net.bluecow.googlecode.ant.GoogleCodeUploadTask")
   upload(username: username, password: password, projectname: "butatopanto", summary: summary, filename: sourcefile, targetfilename: targetfile, labels: "OpSys-All")
 }
 
