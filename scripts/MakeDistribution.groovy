193f2da8dd9d59c14714299c66ab1b86d954ccff
Urs@Urs-PC-Neu <Urs@Urs-PC-Neu>
2010-06-14 17:12:44 +0200

    make-distribution speichert auf Wunsch username und password

--- scripts/MakeDistribution.groovy
+++ scripts/MakeDistribution.groovy
@@ -1,3 +1,5 @@
+import java.util.prefs.Preferences
+
 includeTargets << grailsScript("Init")
 includeTargets << grailsScript("_GrailsWar")
 includeTargets << grailsScript("_GrailsBootstrap")
@@ -8,9 +10,10 @@ target(main: "Builds a war file and publishes it") {
 }
 
 target(uploadToGoogleCode: "Takes the war file and publishes it on the Google page") {
-  def console = System.console();
-  def username = console.readLine("Username? ")
-  def password = console.readLine("Password? ")
+  def console = System.console()
+  def username = readUsername()
+  def password = readPassword()
+  saveUserNameAndPassword(username, password)
   def summary = console.readLine("Content Summary? ")
   def filename = createFilename()
   def sourcefile = "target\\" + filename
@@ -25,10 +28,70 @@ target(uploadToGoogleCode: "Takes the war file and publishes it on the Google pa
           labels: "OpSys-All")
 }
 
+private def readUsername() {
+  def username = loadUsername()
+  if (username) {
+    return username
+  }
+  def console = System.console()
+  console.readLine("Username? ")
+}
+
+private def readPassword() {
+  def password = loadPassword()
+  if (password) {
+    return password
+  }
+  def console = System.console()
+  console.readLine("Password? ")
+}
+
 private def createFilename() {
   loadApp()
   def version = grailsApp.metadata.'app.version'
   "butatopanto-" + version + ".war"
+}
+
+private void saveUserNameAndPassword(def username, def password) {
+  if (userWantsToSave()) {
+    saveUsername username
+    savePassword password
+  }else{
+    saveUsername null
+    savePassword null
+  }
+}
+
+private def userWantsToSave() {
+  def console = System.console()
+  def store = console.readLine("Do you want to store username and password? [y|n] ")
+  store == 'y'
+}
+
+private def loadUsername() {
+  loadPreference('username')
+}
+
+private def loadPassword() {
+  loadPreference('password')
+}
+
+private void saveUsername(def username) {
+  savePreference('username', username)
+}
+
+private void savePassword(def password) {
+  savePreference('password', password)
+}
+
+private def loadPreference(def key) {
+  Preferences userPreferences = Preferences.userRoot();
+  userPreferences.get "butatopanto.distribution.${key}", null
+}
+
+private void savePreference(def key, def value) {
+  Preferences userPreferences = Preferences.userRoot();
+  userPreferences.put "butatopanto.distribution.${key}", value
 }
 
 setDefaultTarget(main)
