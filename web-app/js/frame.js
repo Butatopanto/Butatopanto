b07b96bb3641938000cc34567b99475140a23545
ursreupke@googlemail.com <ursreupke@googlemail.com>
2010-09-05 10:25:12 +0200

    Train.gsp hat ein Beispiel für Story-Anzeige (auskommentiert). Ein Knopf wird bei Fokus gezeigt und bei Blur versteckt, und der Hinweistext wird dabei entfernt oder gesetzt.

---
+++ web-app/js/frame.js
@@ -0,0 +1,26 @@
+function prepareForEntry(nullText) {
+    getSaveButton().style.display = "block";
+    removePlaceholder(nullText);
+}
+function finishEntry(nullText) {
+    getSaveButton().style.display = "none";
+    insertPlaceholder(nullText);
+}
+function getSaveButton() {
+    return document.getElementById("save");
+}
+function removePlaceholder(nullText) {
+    var story = getStoryTextArea();
+    if (story.value == nullText) {
+        story.value = "";
+    }
+}
+function insertPlaceholder(nullText) {
+    var story = getStoryTextArea();
+    if (story.value == "") {
+        story.value = nullText;
+    }
+}
+function getStoryTextArea() {
+    return document.getElementById("story");
+}
