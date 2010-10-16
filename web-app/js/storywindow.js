f04b2b3754290e40fc4cd97ac6b6521b33ca1cec
Sandra@Forest.box <Sandra@Forest.box>
2010-10-16 22:33:38 +0200

    Dialog für Geschichten ist relativ zu dem Link

--- web-app/js/storywindow.js
+++ web-app/js/storywindow.js
@@ -1,5 +1,6 @@
 document.observe('dom:loaded', function() {
   var story = new Control.Modal($('story'), {
+    position: 'relative', 
     overlayOpacity: 0.75,
     className: 'storyWindow',
     fade: true
