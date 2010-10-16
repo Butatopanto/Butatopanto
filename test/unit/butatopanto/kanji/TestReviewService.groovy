135dce0914a1c8753b1e379c9c5cf01a848c65bb
Urs <devnull@localhost>
2010-10-16 15:16:37 +0200

    Ich habe eine ungenutzte Methode entfernt.

--- test/unit/butatopanto/kanji/TestReviewService.groovy
+++ test/unit/butatopanto/kanji/TestReviewService.groovy
@@ -17,8 +17,4 @@ class TestReviewService {
   def getCurrentFrame(Review review) {
     new Frame(meaning: review.currentReview)
   }
-
-  def resolve(Review review, boolean correct){
-    //nothing to do yet
-  }
 }
