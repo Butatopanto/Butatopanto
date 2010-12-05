158984df058986f24915dc597e84163f28bd49fb
Urs <devnull@localhost>
2010-12-05 16:44:57 +0100

    Buta ermittelt die fehlenden Kanji über einen eigenen Service

--- grails-app/services/butatopanto/kanji/bootstrap/HeisigContentService.groovy
+++ grails-app/services/butatopanto/kanji/bootstrap/HeisigContentService.groovy
@@ -2,13 +2,11 @@ package butatopanto.kanji.bootstrap
 
 class HeisigContentService {
 
+  def missingChapterService
+
   def initializeDatabase() {
     log.info "Starting initialization of Heisig frames."
-    def lessons = [
-      new ChapterContent1(), new ChapterContent2(), new ChapterContent3(), new ChapterContent4(), new ChapterContent5(), new ChapterContent6(),
-      new ChapterContent7(), new ChapterContent8(), new ChapterContent9(), new ChapterContent10(), new ChapterContent11(), new ChapterContent12(),
-      new ChapterContent13(), new ChapterContent14(), new ChapterContent15()
-    ]
+    def lessons = missingChapterService.missingChapters()
     lessons.each { it.insertFrames() }
     log.info "Finished initialization of Heisig frames."
   }
