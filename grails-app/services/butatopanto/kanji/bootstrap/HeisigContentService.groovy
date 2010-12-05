d4caa4cf900ab6282afbc892b6c675042b534e5e
Urs <devnull@localhost>
2010-12-05 15:48:00 +0100

    Tippfehler korrigiert

--- grails-app/services/butatopanto/kanji/bootstrap/HeisigContentService.groovy
+++ grails-app/services/butatopanto/kanji/bootstrap/HeisigContentService.groovy
@@ -3,7 +3,7 @@ package butatopanto.kanji.bootstrap
 class HeisigContentService {
 
   def initializeDatabase() {
-    log.info "Starting initialization of Heisig heisig."
+    log.info "Starting initialization of Heisig frames."
     def lessons = [
       new ChapterContent1(), new ChapterContent2(), new ChapterContent3(), new ChapterContent4(), new ChapterContent5(), new ChapterContent6(),
       new ChapterContent7(), new ChapterContent8(), new ChapterContent9(), new ChapterContent10(), new ChapterContent11(), new ChapterContent12(),
