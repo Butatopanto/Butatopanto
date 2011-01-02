65d3896ab7a4f0fefccd2e06da6c95ebfebae43f
Urs <devnull@localhost>
2011-01-02 12:25:19 +0100

    Urs und Sandra sind Administratoren. Ein neuer Benutzer Gast ist es nicht.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -16,13 +16,19 @@ class BootStrap {
   def createUsers() {
     if (!Role.list()) {
       def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+      def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
       String password = springSecurityService.encodePassword('password')
       def urs = new User(username: 'Urs', enabled: true, password: password)
       urs.save(flush: true)
       def sandra = new User(username: 'Sandra', enabled: true, password: password)
       sandra.save(flush: true)
+      def gast = new User(username: 'Gast', enabled: true, password: password)
+      gast.save(flush: true)
       UserRole.create sandra, userRole, true
+      UserRole.create sandra, adminRole, true
       UserRole.create urs, userRole, true
+      UserRole.create urs, adminRole, true
+      UserRole.create gast, userRole, true
     }
   }
 
