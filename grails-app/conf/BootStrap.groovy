c775d5e8bc7c6e0d976bea29c6291c0c41de9141
Urs <devnull@localhost>
2011-01-04 19:06:43 +0100

    Buta legt 'Urs' und 'Sandra' nur noch im Development-Modus an. Im Produktionsmodus gibt es einen Admin-Benutzer.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,7 @@
 import butatopanto.security.Role
 import butatopanto.security.User
 import butatopanto.security.UserRole
-import grails.plugins.springsecurity.SpringSecurityService
+import grails.util.Environment
 
 class BootStrap {
 
@@ -14,22 +14,53 @@ class BootStrap {
   }
 
   def createUsers() {
-    if (!Role.list()) {
-      def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
-      def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-      String password = springSecurityService.encodePassword('password')
-      def urs = new User(username: 'Urs', enabled: true, password: password)
-      urs.save(flush: true)
-      def sandra = new User(username: 'Sandra', enabled: true, password: password)
-      sandra.save(flush: true)
-      def gast = new User(username: 'Gast', enabled: true, password: password)
-      gast.save(flush: true)
-      UserRole.create sandra, userRole, true
-      UserRole.create sandra, adminRole, true
-      UserRole.create urs, userRole, true
-      UserRole.create urs, adminRole, true
-      UserRole.create gast, userRole, true
+    def userRole = loadOrCreateRole('ROLE_USER')
+    def adminRole = loadOrCreateRole('ROLE_ADMIN')
+    createProductionUsers(userRole, adminRole)
+    if (isDevelopment()) {
+      createDevelopmentUsers(userRole, adminRole)
     }
+  }
+
+  private def loadOrCreateRole(def authority) {
+    def role = Role.findByAuthority(authority)
+    if (!role) {
+      role = new Role(authority: authority).save(flush: true)
+    }
+    return role
+  }
+
+  private def isDevelopment() {
+    return Environment.getCurrentEnvironment() == Environment.DEVELOPMENT
+  }
+
+  private def createDevelopmentUsers(Role userRole, Role adminRole) {
+    String password = springSecurityService.encodePassword('password')
+    def urs = new User(username: 'Urs', enabled: true, password: password)
+    urs.save(flush: true)
+    def sandra = new User(username: 'Sandra', enabled: true, password: password)
+    sandra.save(flush: true)
+    def gast = new User(username: 'Gast', enabled: true, password: password)
+    gast.save(flush: true)
+    UserRole.create sandra, userRole, true
+    UserRole.create sandra, adminRole, true
+    UserRole.create urs, userRole, true
+    UserRole.create urs, adminRole, true
+    UserRole.create gast, userRole, true
+  }
+
+  private def createProductionUsers(Role userRole, Role adminRole) {
+    if (!User.findByUsername('Admin')) {
+      createAdmin(userRole, adminRole)
+    }
+  }
+
+  private def createAdmin(Role userRole, Role adminRole) {
+    def password = '0fcd568a5cb9bdb4677b69354b11ee415af8f784519cff3da49a26f84eaee7f2'
+    def admin = new User(username: 'Admin', enabled: true, password: password)
+    admin.save(flush: true)
+    UserRole.create admin, userRole, true
+    UserRole.create admin, adminRole, true
   }
 
   def destroy = {
