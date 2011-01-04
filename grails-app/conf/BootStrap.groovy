2a17abe2cf2dd7c8c8cb0e7b9b8f1c331b6aa3e8
Urs <devnull@localhost>
2011-01-04 19:40:00 +0100

    Development-User auch im Test

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -17,7 +17,7 @@ class BootStrap {
     def userRole = loadOrCreateRole('ROLE_USER')
     def adminRole = loadOrCreateRole('ROLE_ADMIN')
     createProductionUsers(userRole, adminRole)
-    if (isDevelopment()) {
+    if (isDevelopment()||isTest()) {
       createDevelopmentUsers(userRole, adminRole)
     }
   }
@@ -34,18 +34,34 @@ class BootStrap {
     return Environment.getCurrentEnvironment() == Environment.DEVELOPMENT
   }
 
+  private def isTest() {
+    return Environment.getCurrentEnvironment() == Environment.TEST
+  }
+
   private def createDevelopmentUsers(Role userRole, Role adminRole) {
     String password = springSecurityService.encodePassword('password')
+    createUrs(password, userRole, adminRole)
+    createSandra(password, userRole, adminRole)
+    createGuestUser(password, userRole)
+  }
+
+  private def createUrs(String password, Role userRole, Role adminRole) {
     def urs = new User(username: 'Urs', enabled: true, password: password)
     urs.save(flush: true)
-    def sandra = new User(username: 'Sandra', enabled: true, password: password)
-    sandra.save(flush: true)
-    def gast = new User(username: 'Gast', enabled: true, password: password)
-    gast.save(flush: true)
-    UserRole.create sandra, userRole, true
-    UserRole.create sandra, adminRole, true
     UserRole.create urs, userRole, true
     UserRole.create urs, adminRole, true
+  }
+
+  private def createSandra(String password, Role userRole, Role adminRole) {
+    def sandra = new User(username: 'Sandra', enabled: true, password: password)
+    sandra.save(flush: true)
+    UserRole.create sandra, userRole, true
+    UserRole.create sandra, adminRole, true
+  }
+
+  private def createGuestUser(String password, Role userRole) {
+    def gast = new User(username: 'Gast', enabled: true, password: password)
+    gast.save(flush: true)
     UserRole.create gast, userRole, true
   }
 
