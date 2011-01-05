b59b8a3deb26d715c5d16c3da79525e1e6d87d76
\\srvsmb02\H\home\ur\mail <devnull@localhost>
2011-01-05 14:54:40 +0100

    Bootstrap legt Benutzer nur an, wenn ihr Name noch nicht vergeben ist.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -28,54 +28,55 @@ class BootStrap {
   }
 
   private void createAdmin(def roles) {
-    if (!User.findByUsername('Admin')) {
-      def password = '0fcd568a5cb9bdb4677b69354b11ee415af8f784519cff3da49a26f84eaee7f2'
-      def admin = createUser('Admin', password, roles)
-    }
+    def password = '0fcd568a5cb9bdb4677b69354b11ee415af8f784519cff3da49a26f84eaee7f2'
+    def admin = createUser('Admin', password, roles)
   }
+}
 
-  private void createTestUser(def name, def roles) {
-    def password = springSecurityService.encodePassword('password')
-    def user = createUser(name, password, roles)
-  }
+private void createTestUser(def name, def roles) {
+  def password = springSecurityService.encodePassword('password')
+  def user = createUser(name, password, roles)
+}
 
-  private def createUser(name, password, roles) {
+private def createUser(name, password, roles) {
+  if (!User.findByUsername(name)) {
     def user = createUser(name, password)
     grantPermissionsToUser user, roles
   }
+}
 
-  private def createUser(name, password) {
-    def user = new User(username: name, enabled: true, password: password)
-    user.save(flush: true)
-    return user
-  }
+private def createUser(name, password) {
+  def user = new User(username: name, enabled: true, password: password)
+  user.save(flush: true)
+  return user
+}
 
-  private void grantPermissionsToUser(def user, def roles) {
-    roles.each { def role ->
-      UserRole.create user, role, true
-    }
+private void grantPermissionsToUser(def user, def roles) {
+  roles.each { def role ->
+    UserRole.create user, role, true
   }
+}
 
-  private boolean isProduction() {
-    return Environment.getCurrentEnvironment() == Environment.PRODUCTION
-  }
+private boolean isProduction() {
+  return Environment.getCurrentEnvironment() == Environment.PRODUCTION
+}
 
-  private boolean isTest() {
-    return Environment.getCurrentEnvironment() == Environment.TEST
-  }
+private boolean isTest() {
+  return Environment.getCurrentEnvironment() == Environment.TEST
+}
 
-  private boolean isDevelopment() {
-    return Environment.getCurrentEnvironment() == Environment.DEVELOPMENT
-  }
+private boolean isDevelopment() {
+  return Environment.getCurrentEnvironment() == Environment.DEVELOPMENT
+}
 
-  private def loadOrCreateRole(def authority) {
-    def role = Role.findByAuthority(authority)
-    if (!role) {
-      role = new Role(authority: authority).save(flush: true)
-    }
-    return role
+private def loadOrCreateRole(def authority) {
+  def role = Role.findByAuthority(authority)
+  if (!role) {
+    role = new Role(authority: authority).save(flush: true)
   }
+  return role
+}
 
-  def destroy = {
-  }
+def destroy = {
+}
 }
