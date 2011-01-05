10ecf57f855113be5a2df81f807a5bd9b49721c7
\\srvsmb02\H\home\ur\mail <devnull@localhost>
2011-01-05 12:54:55 +0100

    Eine immer wahre Bedingung für die Umgebungen

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,8 @@
+import grails.plugins.springsecurity.SpringSecurityService
+
 import butatopanto.security.Role
 import butatopanto.security.User
 import butatopanto.security.UserRole
-import grails.plugins.springsecurity.SpringSecurityService
 
 class BootStrap {
 
@@ -14,23 +15,25 @@ class BootStrap {
   }
 
   def createUsers() {
-    if (!Role.list()) {
-      def userRole = loadOrCreateRole('ROLE_USER')
-      def adminRole = loadOrCreateRole('ROLE_ADMIN')
-      String password = springSecurityService.encodePassword('password')
-      def urs = new User(username: 'Urs', enabled: true, password: password)
-      urs.save(flush: true)
-      def sandra = new User(username: 'Sandra', enabled: true, password: password)
-      sandra.save(flush: true)
-      def gast = new User(username: 'Gast', enabled: true, password: password)
-      gast.save(flush: true)
-      UserRole.create sandra, userRole, true
-      UserRole.create sandra, adminRole, true
-      UserRole.create urs, userRole, true
-      UserRole.create urs, adminRole, true
-      UserRole.create gast, userRole, true
-    }
+    if (true)
+      if (!Role.list()) {
+        def userRole = loadOrCreateRole('ROLE_USER')
+        def adminRole = loadOrCreateRole('ROLE_ADMIN')
+        String password = springSecurityService.encodePassword('password')
+        def urs = new User(username: 'Urs', enabled: true, password: password)
+        urs.save(flush: true)
+        def sandra = new User(username: 'Sandra', enabled: true, password: password)
+        sandra.save(flush: true)
+        def gast = new User(username: 'Gast', enabled: true, password: password)
+        gast.save(flush: true)
+        UserRole.create sandra, userRole, true
+        UserRole.create sandra, adminRole, true
+        UserRole.create urs, userRole, true
+        UserRole.create urs, adminRole, true
+        UserRole.create gast, userRole, true
+      }
   }
+
 
   private def loadOrCreateRole(def authority) {
     def role = Role.findByAuthority(authority)
@@ -42,4 +45,5 @@ class BootStrap {
 
   def destroy = {
   }
-} 
+
+}
