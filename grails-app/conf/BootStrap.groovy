9b3dc27f6a0da8cb2108ad8ca1117f200d390b80
\\srvsmb02\H\home\ur\mail <devnull@localhost>
2011-01-05 14:59:55 +0100

    Rollen vor dem IF

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -14,16 +14,14 @@ class BootStrap {
   }
 
   def createUsers() {
+    def userRole = loadOrCreateRole('ROLE_USER')
+    def adminRole = loadOrCreateRole('ROLE_ADMIN')
     if (isDevelopment() || isTest() || isProduction()) {
-      if (!Role.list()) {
-        def userRole = loadOrCreateRole('ROLE_USER')
-        def adminRole = loadOrCreateRole('ROLE_ADMIN')
-        def allRoles = [userRole, adminRole]
-        createTestUser('Urs', allRoles)
-        createTestUser('Sandra', allRoles)
-        createTestUser('Gast', [userRole])
-        createAdmin(allRoles)
-      }
+      def allRoles = [userRole, adminRole]
+      createTestUser('Urs', allRoles)
+      createTestUser('Sandra', allRoles)
+      createTestUser('Gast', [userRole])
+      createAdmin(allRoles)
     }
   }
 
@@ -31,52 +29,51 @@ class BootStrap {
     def password = '0fcd568a5cb9bdb4677b69354b11ee415af8f784519cff3da49a26f84eaee7f2'
     def admin = createUser('Admin', password, roles)
   }
-}
 
-private void createTestUser(def name, def roles) {
-  def password = springSecurityService.encodePassword('password')
-  def user = createUser(name, password, roles)
-}
-
-private def createUser(name, password, roles) {
-  if (!User.findByUsername(name)) {
-    def user = createUser(name, password)
-    grantPermissionsToUser user, roles
+  private void createTestUser(def name, def roles) {
+    def password = springSecurityService.encodePassword('password')
+    def user = createUser(name, password, roles)
   }
-}
 
-private def createUser(name, password) {
-  def user = new User(username: name, enabled: true, password: password)
-  user.save(flush: true)
-  return user
-}
-
-private void grantPermissionsToUser(def user, def roles) {
-  roles.each { def role ->
-    UserRole.create user, role, true
+  private def createUser(name, password, roles) {
+    if (!User.findByUsername(name)) {
+      def user = createUser(name, password)
+      grantPermissionsToUser user, roles
+    }
   }
-}
 
-private boolean isProduction() {
-  return Environment.getCurrentEnvironment() == Environment.PRODUCTION
-}
-
-private boolean isTest() {
-  return Environment.getCurrentEnvironment() == Environment.TEST
-}
-
-private boolean isDevelopment() {
-  return Environment.getCurrentEnvironment() == Environment.DEVELOPMENT
-}
-
-private def loadOrCreateRole(def authority) {
-  def role = Role.findByAuthority(authority)
-  if (!role) {
-    role = new Role(authority: authority).save(flush: true)
+  private def createUser(name, password) {
+    def user = new User(username: name, enabled: true, password: password)
+    user.save(flush: true)
+    return user
   }
-  return role
-}
 
-def destroy = {
-}
+  private void grantPermissionsToUser(def user, def roles) {
+    roles.each { def role ->
+      UserRole.create user, role, true
+    }
+  }
+
+  private boolean isProduction() {
+    return Environment.getCurrentEnvironment() == Environment.PRODUCTION
+  }
+
+  private boolean isTest() {
+    return Environment.getCurrentEnvironment() == Environment.TEST
+  }
+
+  private boolean isDevelopment() {
+    return Environment.getCurrentEnvironment() == Environment.DEVELOPMENT
+  }
+
+  private def loadOrCreateRole(def authority) {
+    def role = Role.findByAuthority(authority)
+    if (!role) {
+      role = new Role(authority: authority).save(flush: true)
+    }
+    return role
+  }
+
+  def destroy = {
+  }
 }
