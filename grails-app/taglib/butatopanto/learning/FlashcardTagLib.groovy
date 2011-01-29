61d13b88037ce32cea2d75c931e4615a88dd42c8
Urs <devnull@localhost>
2011-01-29 20:29:41 +0100

    Das Diagramm wird eleganter generiert.

--- grails-app/taglib/butatopanto/learning/FlashcardTagLib.groovy
+++ grails-app/taglib/butatopanto/learning/FlashcardTagLib.groovy
@@ -11,20 +11,18 @@ class FlashcardTagLib {
   }
 
   def generateDataScript = {arguments ->
-    def boxes = arguments.boxes
     def data = arguments.dataVariable
-    def firstEntry = LeitnerService.FIRST_BOX - 1
-    def lastEntry = LeitnerService.LAST_BOX - 1
     out << "${data}.addColumn('string', 'Box');"
     out << "${data}.addColumn('number', 'Due');"
     out << "${data}.addColumn('number', 'Mastered');"
-    out << "${data}.addRows(${boxes.size()});"
-    for (index in firstEntry..lastEntry) {
-      def box = boxes[index]
-      def roman = romanNumber(number: box.number)
-      out << "data.setValue($index, 0, '$roman');"
-      out << "data.setValue($index, 1, ${box.dueKanji});"
-      out << "data.setValue($index, 2, ${box.masteredKanji});"
+    int index = 0
+    arguments.boxes.each {
+      def roman = romanNumber(number: it.number)
+      out << "${data}.addRow();"
+      out << "${data}.setValue($index, 0, '$roman');"
+      out << "${data}.setValue($index, 1, ${it.dueKanji});"
+      out << "${data}.setValue($index, 2, ${it.masteredKanji});"
+      index++
     }
   }
 }
