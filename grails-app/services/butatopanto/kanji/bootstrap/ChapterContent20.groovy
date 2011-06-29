01f98975119ff75ae15ee59627a8f31f0a570451
Urs <devnull@localhost>
2011-06-29 20:01:44 +0200

    Kapitel 20, 21

---
+++ grails-app/services/butatopanto/kanji/bootstrap/ChapterContent20.groovy
@@ -0,0 +1,18 @@
+package butatopanto.kanji.bootstrap
+
+class ChapterContent20 extends ChapterContent {
+
+  ChapterContent20() {
+    super(20)
+  }
+
+  @Override
+  void addFramesToDatabase() {
+    insertFrame(509, "染", "färben")
+    insertFrame(510, "燃", "brennen")
+    insertFrame(511, "賓", "Ehrengast")
+    insertFrame(512, "歳", "Jahresende")
+    insertFrame(513, "県", "Präfektur")
+    insertFrame(514, "栃", "Rosskastanie")
+  }
+}
