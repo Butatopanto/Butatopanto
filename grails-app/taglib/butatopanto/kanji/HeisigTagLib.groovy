4516cd184d123e98834866709257eb77858b2f1f
\\srvsmb02\H\home\ur\mail <devnull@localhost>
2011-07-07 10:26:14 +0200

    Und schließlich: Die Knöpfe im Review sind zu erkennen.

--- grails-app/taglib/butatopanto/kanji/HeisigTagLib.groovy
+++ grails-app/taglib/butatopanto/kanji/HeisigTagLib.groovy
@@ -54,9 +54,9 @@ class HeisigTagLib {
     "<div class='frameMessage'>" +
             "<p>${g.message(code: 'frame.reviewResultQuestion')}</p>" +
             g.form(name: 'reviewKanji') {
-              g.submitToRemote(onComplete: 'closeStoryDialog()', id: 'confirmButton', class: "confirm", update: 'container', value: g.message(code: 'frame.reviewResult.confirm'), url: [controller: 'review', action: 'ajaxResolve', params: [kanji: frame.kanji, reviewCorrect: true]]) +
+              g.submitToRemote(onComplete: 'closeStoryDialog()', id: 'confirmButton', class: "medium green button", update: 'container', value: g.message(code: 'frame.reviewResult.confirm'), url: [controller: 'review', action: 'ajaxResolve', params: [kanji: frame.kanji, reviewCorrect: true]]) +
                       " " +
-                      g.submitToRemote(onComplete: 'closeStoryDialog()', id: 'declineButton', class: "decline", update: 'container', value: g.message(code: 'frame.reviewResult.decline'), url: [controller: 'review', action: 'ajaxResolve', params: [kanji: frame.kanji, reviewCorrect: false]])
+                      g.submitToRemote(onComplete: 'closeStoryDialog()', id: 'declineButton', class: "medium red button", update: 'container', value: g.message(code: 'frame.reviewResult.decline'), url: [controller: 'review', action: 'ajaxResolve', params: [kanji: frame.kanji, reviewCorrect: false]])
             } +
             "</div>"
   }
