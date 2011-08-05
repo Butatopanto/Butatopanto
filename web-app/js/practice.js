795c314b246c520e9f7aec16ff82c521c2739a7e
Urs <devnull@localhost>
2011-08-05 18:19:02 +0200

    Besseres JS

--- web-app/js/practice.js
+++ web-app/js/practice.js
@@ -1,11 +1,11 @@
 var storyWin;
 
 function openStoryDialog(html, url, title) {
-  storyWin = new Window({title: title, url: url, className:"story", parent:$('showStory'), draggable:false})
+  storyWin = new Window({title: title, url: url, className:"story", parent:$('showStory'), draggable:false});
   storyWin.setDestroyOnClose();
-  storyWin.setConstraint(true, {top:0, left: 0, right:0, bottom:0})
+  storyWin.setConstraint(true, {top:0, left: 0, right:0, bottom:0});
   storyWin.show();
-  storyWin.maximize()
+  storyWin.maximize();
 }
 
 function closeStoryDialog() {
