1a0487be4aa791bb75101503f0fdfc4c608b4888
Urs <devnull@localhost>
2011-08-05 18:43:08 +0200

    Der Story-Dialog zeigt sich auch bei den Kapiteln wieder schön.

--- web-app/js/chapterlist.js
+++ web-app/js/chapterlist.js
@@ -1,4 +1,4 @@
 function openStoryDialog(html, title, url) {
-    var win = new Window({className: "story", url: url,  width: 215, height: 270, zIndex: 100,  title: title, draggable:true, wiredDrag: true})
+    var win = new Window({className: "story", url: url,  width: 230, height: 290, zIndex: 100,  title: title, draggable:true, wiredDrag: true})
     win.showCenter(true);
 }
