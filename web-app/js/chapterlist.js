d702e1e634f7cc38e5372ebaba72f0fc2b5e5358
Urs Reupke <ursreupke@gmail.com>
2011-09-07 22:17:23 +0200

    Removed unused JavaScript * to open old story dialog * framework for old story dialog * old mousewheel handler * ancient kanji-revealer

--- web-app/js/chapterlist.js
+++
@@ -1,4 +0,0 @@
-function openStoryDialog(html, title, url) {
-    var win = new Window({className: "story", url: url,  width: 230, height: 290, zIndex: 100,  title: title, draggable:true, wiredDrag: true})
-    win.showCenter(true);
-}
