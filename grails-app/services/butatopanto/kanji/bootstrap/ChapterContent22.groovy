dd1245ff896e845ad71deccde1375db0b743a080
Urs Reupke <ursreupke@gmail.com>
2011-10-04 21:37:35 +0200

    Kapitel 22 bis Höhle

---
+++ grails-app/services/butatopanto/kanji/bootstrap/ChapterContent22.groovy
@@ -0,0 +1,29 @@
+package butatopanto.kanji.bootstrap
+
+class ChapterContent22 extends ChapterContent{
+
+  ChapterContent22(){
+    super(21)
+  }
+
+  @Override
+  void addFramesToDatabase() {
+    insertFrame(578, "曰", "zu sagen pflegen")
+    insertFrame(579, "困", "in Verlegenheit")
+    insertFrame(580, "固", "hart")
+    insertFrame(581, "国", "Land")
+    insertFrame(582, "団", "Gruppe")
+    insertFrame(583, "因", "Ursache")
+    insertFrame(584, "姻	", "Ehe")
+    insertFrame(585, "園", "Park")
+    insertFrame(586, "回", "x-mal")
+    insertFrame(587, "壇", "Podium")
+    insertFrame(588, "店", "Laden")
+    insertFrame(589, "庫", "Lagerhaus")
+    insertFrame(590, "庭", "Garten")
+    insertFrame(591, "庁", "Ministerium")
+    insertFrame(592, "床", "Bett")
+    insertFrame(593, "麻", "Hanf")
+    insertFrame(594, "磨", "polieren")
+  }
+}
