9cfd61cef9fd33b8493b90e5ba1f854d943cbcd8
Urs Reupke <ursreupke@gmail.com>
2011-10-04 22:27:16 +0200

    Chapter 22 done

--- grails-app/services/butatopanto/kanji/bootstrap/ChapterContent22.groovy
+++ grails-app/services/butatopanto/kanji/bootstrap/ChapterContent22.groovy
@@ -1,38 +1,78 @@
 package butatopanto.kanji.bootstrap
 
-class ChapterContent22 extends ChapterContent{
+class ChapterContent22 extends ChapterContent {
 
-  ChapterContent22(){
+  private static final int StartOfChapter = 578;
+  private int nextNumber = StartOfChapter;
+
+  ChapterContent22() {
     super(21)
   }
 
   @Override
   void addFramesToDatabase() {
-    insertFrame(578, "曰", "zu sagen pflegen")
-    insertFrame(579, "困", "in Verlegenheit")
-    insertFrame(580, "固", "hart")
-    insertFrame(581, "国", "Land")
-    insertFrame(582, "団", "Gruppe")
-    insertFrame(583, "因", "Ursache")
-    insertFrame(584, "姻	", "Ehe")
-    insertFrame(585, "園", "Park")
-    insertFrame(586, "回", "x-mal")
-    insertFrame(587, "壇", "Podium")
-    insertFrame(588, "店", "Laden")
-    insertFrame(589, "庫", "Lagerhaus")
-    insertFrame(590, "庭", "Garten")
-    insertFrame(591, "庁", "Ministerium")
-    insertFrame(592, "床", "Bett")
-    insertFrame(593, "麻", "Hanf")
-    insertFrame(594, "磨", "polieren")
-    insertFrame(595, "心", "Herz")
-    insertFrame(596, "忘", "vergessen")
-    insertFrame(597, "忍", "erdulden")
-    insertFrame(598, "認", "anerkennen")
-    insertFrame(599, "忌", "Trauer")
-    insertFrame(600, "志", "Absicht")
-    insertFrame(601, "誌", "Zeitschrift")
-    insertFrame(602, "忠", "Treue")
-    insertFrame(603, "串", "Fleischspieß")
+    addKanji("曰", "zu sagen pflegen")
+    addKanji("困", "in Verlegenheit")
+    addKanji("固", "hart")
+    addKanji("国", "Land")
+    addKanji("団", "Gruppe")
+    addKanji("因", "Ursache")
+    addKanji("姻	", "Ehe")
+    addKanji("園", "Park")
+    addKanji("回", "x-mal")
+    addKanji("壇", "Podium")
+    addKanji("店", "Laden")
+    addKanji("庫", "Lagerhaus")
+    addKanji("庭", "Garten")
+    addKanji("庁", "Ministerium")
+    addKanji("床", "Bett")
+    addKanji("麻", "Hanf")
+    addKanji("磨", "polieren")
+    addKanji("心", "Herz")
+    addKanji("忘", "vergessen")
+    addKanji("忍", "erdulden")
+    addKanji("認", "anerkennen")
+    addKanji("忌", "Trauer")
+    addKanji("志", "Absicht")
+    addKanji("誌", "Zeitschrift")
+    addKanji("忠", "Treue")
+    addKanji("串", "befallen")
+    addKanji("思", "denken")
+    addKanji("恩", "Gunst")
+    addKanji("応", "erwidern")
+    addKanji("意", "Wille")
+    addKanji("想", "Idee")
+    addKanji("息", "Atem")
+    addKanji("憩", "Rast")
+    addKanji("恵", "Gnade")
+    addKanji("恐", "Furcht")
+    addKanji("惑", "unsicher")
+    addKanji("感", "Gefühl")
+    addKanji("憂", "Trübsinn")
+    addKanji("憂", "verwitwet")
+    addKanji("忙", "beschäftigt")
+    addKanji("悦", "Entzücken")
+    addKanji("恒", "beständig")
+    addKanji("悼", "beklagen")
+    addKanji("悟", "Erleuchtung")
+    addKanji("怖", "schrecklich")
+    addKanji("慌", "überstürzt")
+    addKanji("悔", "bereuen")
+    addKanji("憎", "hassen")
+    addKanji("慣", "Gewohnheit")
+    addKanji("愉", "Vergmügen")
+    addKanji("惰", "faul")
+    addKanji("慎", "Mäßigung")
+    addKanji("憾", "Bedauern")
+    addKanji("憶", "Gedächtnis")
+    addKanji("慕", "Sehnsucht")
+    addKanji("添", "beifügen")
+    addKanji("必", "zwangsläufig")
+    addKanji("泌", "absondern")
+  }
+
+  private addKanji(kanji, keyword){
+    insertFrame(nextNumber, kanji, keyword)
+    nextNumber++;
   }
 }
